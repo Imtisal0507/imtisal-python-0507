bac7e8845cb024f0feee337945479cb6d3eaa8a1
Imtisal0507 <ansariimtisal@gmail.com>
2024-08-20 09:09:19 +0530

    day 1

---
+++ classwork/20-08-2024/basic of python.py
@@ -0,0 +1,13 @@
+a=10
+b=20
+
+print("Hello")
+print("World")
+
+print("A :",a)
+print("B :",b)
+
+print("Addition : ",a+b)
+print("Substraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
---
+++ classwork/20-08-2024/calculator byy python.py
@@ -0,0 +1,11 @@
+a = float(input("Enter value :"))
+b = int(input("Enter value :"))
+#type casting
+
+print(type(a))
+print(type(b))
+
+print("Addiion :",a+b)
+print("Substraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
